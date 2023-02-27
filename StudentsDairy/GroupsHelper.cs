f12abc23577947019d0245825f72320f2a026b8e
Michał Sarnacki <dzon444@outlook.com>
2023-02-27 19:03:58 +0100

    changes in GroupsHelper

--- StudentsDairy/GroupsHelper.cs
+++ StudentsDairy/GroupsHelper.cs
@@ -14,11 +14,11 @@ namespace StudentsDairy
             {
             new Group { Id = 0, Name = defaultGroup },
             new Group { Id = 1, Name = "1a" },
-            new Group { Id = 1, Name = "1b" },
-            new Group { Id = 1, Name = "2a" },
-            new Group { Id = 2, Name = "2b" },
-            new Group { Id = 1, Name = "3a" },
-            new Group { Id = 2, Name = "3b" }
+            new Group { Id = 2, Name = "1b" },
+            new Group { Id = 3, Name = "2a" },
+            new Group { Id = 4, Name = "2b" },
+            new Group { Id = 5, Name = "3a" },
+            new Group { Id = 6, Name = "3b" }
             };
             
         }
